9c88c716afe302d12ade746ebd0273bbee592e43
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-02-26 09:39:57 +0530

    Player Movement with mouse look

---
+++ Wave Destroyer/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Camera cam;
+    PlayerController controller;
+
+    [Header("Inputs")]
+    [SerializeField] private Vector2 moveInput;
+    [SerializeField] private Vector2 mousePos;
+
+    private void Awake()
+    {
+        controller = new PlayerController();
+        Movement();
+    }
+
+    void Movement()
+    {
+        controller.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controller.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        controller.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controller.Player.Disable();
+    }
+
+    private void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 move = rb.position + moveInput * moveSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(move);
+
+        MouseLook();
+    }
+
+    void MouseLook()
+    {
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
